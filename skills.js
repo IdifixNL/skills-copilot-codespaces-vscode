e1cd66cc24452b338acbb154f099d3fa19a80c60
IdifixNL <nico_baburek@hotmail.com>
2024-05-02 21:39:01 +0000

    Copilot

--- skills.js
+++ skills.js
@@ -1,1 +1,6 @@
-function calculateNumbers(var1, var2) 
+function calculateNumbers(var1, var2) {
+    if(typeof var1 !== 'number' || typeof var2 !== 'number') {
+        throw new Error('Both inputs must be numbers');
+    }
+    return var1 + var2;
+}
